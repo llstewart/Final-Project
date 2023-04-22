342471493588f330810ea5dd70cfaf925bdd19af
llstewart <lincolnstewart4@gmail.com>
2023-04-21 22:18:53 -0400

    updated test cases

--- test_final_project.py
+++ test_final_project.py
@@ -1,11 +1,20 @@
 import unittest
 from Final_Project import NBAPlayerData  # Assuming the original code is saved in a file named Final_Project.py
+"""
+This module contains unit tests for the NBAPlayerData class in the Final_Project.py file.
+"""
 
 class TestNBAPlayerData(unittest.TestCase):
 
+    """
+        Set up the test fixture by instantiating the NBAPlayerData object.
+     """
     def setUp(self):
         self.player_data = NBAPlayerData()
 
+    """
+        Test the get_player_id method by checking the return values for a known player and a nonexistent player.
+        """
     def test_get_player_id(self):
         player_id = self.player_data.get_player_id("Lebron James")
         self.assertIsNotNone(player_id)
@@ -14,6 +23,9 @@ class TestNBAPlayerData(unittest.TestCase):
         player_id = self.player_data.get_player_id("Nonexistent Player")
         self.assertIsNone(player_id)
 
+        """
+        Test the get_season_averages method by checking the return values for valid and invalid seasons.
+        """
     def test_get_season_averages(self):
         lebron_id = 2544
         season_averages = self.player_data.get_season_averages(lebron_id, 2020)
@@ -23,6 +35,9 @@ class TestNBAPlayerData(unittest.TestCase):
         season_averages = self.player_data.get_season_averages(lebron_id, 1999)
         self.assertIsNone(season_averages)
 
+    """
+        Test the get_player_data method by checking the return values for valid and invalid inputs.
+        """
     def test_get_player_data(self):
         player_data = self.player_data.get_player_data("Lebron James", 2020)
         self.assertIsNotNone(player_data)
